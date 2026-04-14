1784247b978ceec5e7860e02002e87986027f3bf
imbipin <bipinka01@gmail.com>
2026-04-14 12:22:18 +0545

    fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-// src/App.tsx
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const App = () => (
-  <BrowserRouter basename="/portfolio">
+  <BrowserRouter>
     <Routes>
-      {/* This handles the main page */}
       <Route path="/" element={<Index />} />
-      
-      {/* This is your safety net. 
-          If you click a button and the URL changes to /about, 
-          this 'path="*"' catches it and shows the error.
-          By using the Navbar code above, we prevent this from ever being triggered.
-      */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
